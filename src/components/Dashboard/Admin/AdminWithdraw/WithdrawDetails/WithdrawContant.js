cfd7b988f18bd825c303666d7b2564c8c1ec60ee
akramulratul <68425589+akramulratul@users.noreply.github.com>
2021-06-26 00:35:36 +0600

    decline button

--- src/components/Dashboard/Admin/AdminWithdraw/WithdrawDetails/WithdrawContant.js
+++ src/components/Dashboard/Admin/AdminWithdraw/WithdrawDetails/WithdrawContant.js
@@ -1,6 +1,10 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 const WithdrawContant = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div className="dashboard-container py-5 px-4">
       <div className="row d-flex">
@@ -17,18 +21,48 @@ const WithdrawContant = () => {
             <h6>Phone Number:</h6>
           </div>
         </div>
-        <div>
-          <div className="col-lg-4 admin-right-area bg-white">
+        <div className="col-lg-4 admin-right-area bg-white">
+          <div>
             <h4 className="border-bottom">Withdrawal By</h4>
             <h6>USER</h6>
           </div>
           <div>
-            <div className="btn btn-brand w-100 py-2 mt-2" onClick="">
+            <Button className="btn btn-brand w-100 py-2 mt-2">
               <h6>Approve</h6>
-            </div>
-            <div className="btn btn-brand w-100 py-2 mt-2" onClick="">
+            </Button>
+            <Button
+              className="btn btn-brand w-100 py-2 mt-2 button-decline "
+              onClick={handleShow}
+            >
               <h6>Decline</h6>
-            </div>
+            </Button>
+          </div>
+          <div>
+            <Modal show={show} onHide={handleClose} animation={false}>
+              <Modal.Header closeButton>
+                <Modal.Title>Are you confirm?</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                You want to Decline This withdrawal. if you confirm then press
+                the decline button.
+              </Modal.Body>
+              <Modal.Footer>
+                <Button
+                  className="button-decline"
+                  variant="secondary"
+                  onClick={handleClose}
+                >
+                  Decline
+                </Button>
+                <Button
+                  className="btn w-250"
+                  variant="primary"
+                  onClick={handleClose}
+                >
+                  Cancel
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </div>
         </div>
       </div>
