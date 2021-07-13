c144392f2cc4d572e0ea3d6249c2095f146d0044
akramulratul <68425589+akramulratul@users.noreply.github.com>
2021-07-13 06:11:05 +0600

    setting page

--- src/components/Dashboard/Admin/AdminSetting/AdminSetting.js
+++ src/components/Dashboard/Admin/AdminSetting/AdminSetting.js
@@ -1,14 +1,18 @@
 import React from "react";
 import AdminSidebar from "../AdminSidebar";
 import SettingTop from "./SettingTop";
+import DashboardNav from "../../DashboardNav";
+import SettingContent from "./SettingContent";
 const AdminSetting = () => {
   return (
     <div className="row mr-0">
       <div className="col-lg-3 p-0">
         <AdminSidebar />
       </div>
-      <div className="col-lg-9 p-0">
+      <div className="incentive-background col-lg-9 p-0">
+        <DashboardNav />
         <SettingTop />
+        <SettingContent />
       </div>
     </div>
   );
---
+++ src/components/Dashboard/Admin/AdminSetting/SettingContent.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SettingContent = () => {
+  return (
+    <div className="bg-white justify-content-between rounded mt-3 ml-3 mr-3">
+      <div>
+        <h1>General Settings</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          maxime unde rerum!
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default SettingContent;
