11783d5c401cc770077caff9db8376d61376c79c
Tanvir Sazib <tansazib@gmail.com>
2021-07-15 23:42:22 +0600

    home page responsive

--- src/components/Redux/Actions/withdrawAction.js
+++ src/components/Redux/Actions/withdrawAction.js
@@ -13,7 +13,7 @@ export const addWithdraw = (newWithdrawInfo) => async (dispatch) => {
       payload: {},
     });
 
-    fetch("https://utopain-backend.herokuapp.com/user/balance/withdraws", {
+    fetch("http://api.utopiansglobal.com/user/balance/withdraws", {
       method: "POST",
       headers: {
         "content-type": "application/json",
@@ -81,7 +81,7 @@ export const getWithdrawHistory = () => async (dispatch) => {
     };
 
     const { data } = await axios.get(
-      "https://utopain-backend.herokuapp.com/user/balance/withdraws",
+      "http://api.utopiansglobal.com/user/balance/withdraws",
       config
     );
     dispatch({
@@ -112,7 +112,7 @@ export const sendOtp = () => async (dispatch) => {
     });
 
     fetch(
-      "https://utopain-backend.herokuapp.com/user/balance/withdraws/generate/otp",
+      "http://api.utopiansglobal.com/user/balance/withdraws/generate/otp",
       {
         method: "POST",
         headers: {
