bbe38e0a5d6b39b64bd0de16f91d42b56c022999
unknown <ashikbhuiyan.swe.diu@gmail.com>
2021-09-13 00:28:52 +0600

    adminIncentive,adminTeamIncentive,TopIncentive

--- src/components/Redux/Actions/incentiveAction.js
+++ src/components/Redux/Actions/incentiveAction.js
@@ -130,3 +130,134 @@ export const adminIncentiveDecline = (declineData) => async (dispatch) => {
     });
   }
 };
+
+
+export const incentiveTeamHistoryAction = () => async (dispatch) => {
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const {
+    data: { token },
+  } = userInfo;
+
+  try {
+    dispatch({
+      type: "INCENTIVE_HISTORY_REQUEST",
+      payload: {},
+    });
+
+    const config = {
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.get(
+      "http://api.utopiansglobal.com/admin/teamIncentives",
+      config
+    );
+    dispatch({
+      type: "INCENTIVE_HISTORY_SUCCESS",
+      payload: data.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: "INCENTIVE_HISTORY_FAIL",
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const adminTeamIncentiveApproved = (approvedData) => async (dispatch) => {
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const {
+    data: { token },
+  } = userInfo;
+
+  try {
+    dispatch({
+      type: "ADMIN_INCENTIVE_APPROVED_REQUEST",
+      payload: {},
+    });
+
+    const config = {
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+    };
+    const statusData = {
+      status: approvedData.status,
+    };
+
+    const { data } = await axios.post(
+      `http://api.utopiansglobal.com/admin/teamIncentives/${approvedData.id}/manage`,
+      statusData,
+      config
+    );
+    console.log(data);
+    dispatch({
+      type: "ADMIN_INCENTIVE_APPROVED_SUCCESS",
+      payload: data.data,
+    });
+    if (data.data) {
+      window.location.reload();
+    }
+  } catch (error) {
+    dispatch({
+      type: "ADMIN_INCENTIVE_APPROVED_FAIL",
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const adminTeamIncentiveDecline = (declineData) => async (dispatch) => {
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const {
+    data: { token },
+  } = userInfo;
+
+  try {
+    dispatch({
+      type: "ADMIN_INCENTIVE_DECLINE_REQUEST",
+      payload: {},
+    });
+
+    const config = {
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+    };
+    const statusData = {
+      status: declineData.status,
+    };
+
+    const { data } = await axios.post(
+      `http://api.utopiansglobal.com/admin/teamIncentives/${declineData.id}/manage`,
+      statusData,
+      config
+    );
+    console.log(data);
+    dispatch({
+      type: "ADMIN_INCENTIVE_DECLINE_SUCCESS",
+      payload: data.data,
+    });
+    if (data.data) {
+      window.location.reload();
+    }
+  } catch (error) {
+    dispatch({
+      type: "ADMIN_INCENTIVE_DECLINE_FAIL",
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
